fb81c547c8425522eec186eb10fd3c2f95437361
Jules Hedges <juleshedges.invariant@gmail.com>
2024-10-30 14:43:25 +0000

    Monte carlo decision operator

---
+++ src/OpenGames/Engine/MC.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+
+module OpenGames.Engine.MC where
+
+-- Adapted from https://github.com/philipp-zahn/open-games-engine/blob/monte-carlo-monad-bayes/src/OpenGames/Engine/BayesianMC.hs
+-- but (1) with the state hack removed for efficiency,
+-- and (2) with a decision operator that just forwards payoffs to an external optimiser
+
+import Control.Arrow
+
+import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Population
+import Control.Monad.Bayes.Sampler.Strict
+
+import OpenGames.Engine.OpenGames
+import OpenGames.Engine.MonadicOptics
+import OpenGames.Engine.TLL
+
+type MCIO = PopulationT SamplerIO
+
+-- TODO change evaluate to send n samples
+decisionMC :: (Monad m)
+           => Int
+           -> OpenGame (MonadicOptic m)
+                       (MonadicContext m)
+                       '[x -> m y]
+                       '[m [r]]
+                       x () y r
+decisionMC numSamples = OpenGame {
+    play = \(f ::- Nil) 
+      -> let v x = do {y <- f x; pure ((), y)}
+             u () _ = pure ()
+          in MonadicOptic v u
+  , evaluate = \(f ::- Nil) (MonadicContext h k)
+      -> let sample = do {(z, x) <- h; y <- f x; k z y}
+             result = sequence (replicate numSamples sample)
+          in result ::- Nil
+}
